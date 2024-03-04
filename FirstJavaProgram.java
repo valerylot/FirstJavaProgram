48b5870a1677cde2dd5c46bfcdd6592021ecae94
valerylot <lotvalery@gmail.com>
2024-03-04 11:36:14 -0800

    first commit

---
+++ FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram{
+    public static void main(String[] args){
+        System.out.println("My name is Valery Lot.");
+        System.out.println("I am 29 years old.");
+        System.out.println("My hometown is Stockton, CA.");
+    }
+}
